46d104a3fc21f2ae9614984f73203ee5242995d1
yangzong18 <183851063@qq.com>
2019-08-27 17:15:48 +0800

    first commit

---
+++ src/views/Register.vue
@@ -0,0 +1,103 @@
+<template>
+  <el-form :model="ruleForm2" ref="ruleForm2" :rules="rules2" label-width="80px" class="demo-from register-container">
+    <h3 class="title">系统注册</h3>
+    <el-form-item label="账号">
+      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item label="密码">
+      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="账号"></el-input>
+    </el-form-item>
+    <el-form-item><el-checkbox v-model="checked" checked class="remember">记住密码</el-checkbox></el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click.native.prevent="handleSubmit2" :loading="logining">立即注册</el-button>
+      <el-button @click.native.prevent="handleReset2">重置</el-button>
+    </el-form-item>
+  </el-form>
+  
+</template>
+
+<script>
+  import { requestRegister } from '../api/api';
+  //import NProgress from 'nprogress'
+  export default {
+    data() {
+      return {
+        logining: false,
+        ruleForm2: {
+          account: 'admin',
+          checkPass: '123456'
+        },
+        rules2: {
+          account: [
+            { required: true, message: '请输入账号', trigger: 'blur' },
+            //{ validator: validaePass }
+          ],
+          checkPass: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            //{ validator: validaePass2 }
+          ]
+        },
+        checked: true
+      };
+    },
+    methods: {
+      handleReset2() {
+        this.$refs.ruleForm2.resetFields();
+      },
+      handleSubmit2(ev) {
+        var _this = this;
+        this.$refs.ruleForm2.validate((valid) => {
+          if (valid) {
+            //_this.$router.replace('/table');
+            this.logining = true;
+            //NProgress.start();
+            var params = { username: this.ruleForm2.account, password: this.ruleForm2.checkPass };
+            requestRegister(params).then(data => {
+              this.logining = false;
+              //NProgress.done();
+              let { msg, code, user } = data;
+              if (code !== 200) {
+                this.$message({
+                  message: msg,
+                  type: 'error'
+                });
+              } else {
+                sessionStorage.setItem('user', JSON.stringify(user));
+                this.$router.push({ path: '/table' });
+              }
+            });
+          } else {
+            console.log('error submit!!');
+            return false;
+          }
+        });
+      }
+    }
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  .register-container {
+    /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+    -moz-border-radius: 5px;
+    background-clip: padding-box;
+    margin: 180px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+    .title {
+      margin: 0px auto 40px auto;
+      text-align: center;
+      color: #505458;
+    }
+    .remember {
+      margin: 0px 0px 35px 0px;
+    }
+    
+  }
+</style>
